7251b7ad27d4b798dcd756e1ad9325456c4d7f2f
Daniel Muñoz <dmunozb@outlook.es>
2023-06-03 00:00:37 -0400

    chore: version bump only

--- rancilio-pid/rancilio-pid.ino
+++ rancilio-pid/rancilio-pid.ino
@@ -21,7 +21,7 @@
 
 RemoteDebug Debug;
 
-const char* sysVersion PROGMEM = "3.2.2-beta1";
+const char* sysVersion PROGMEM = "3.2.2-beta7";
 
 /********************************************************
  * definitions below must be changed in the userConfig.h file
